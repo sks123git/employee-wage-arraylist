6050117e209e1fd02adb72b67523a79749652f52
subzero <shubham8111994@gmail.com>
2023-02-13 21:14:35 +0530

    added employee total wage till no of wrking hours and days

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -3,11 +3,12 @@ public class EmployeeWageBuilder {
     public static final int IS_PART_TIME =2;
     public static final int EMP_RATE_PER_HOUR=20;
     public static final int NO_OF_WORKING_DAYS=20;
+    public static final int MAX_HOURS_PER_MONTH= 100;
     public static void main(String[] args) {
         int empHrs=0;
-        int empWage=0;
-        int totalEmpWage = 0;
-        for (int i =0; i<NO_OF_WORKING_DAYS; i++) {
+        int totalEmpHours = 0;
+        int totalWorkingDays=0;
+        while (totalEmpHours<=MAX_HOURS_PER_MONTH && totalWorkingDays<NO_OF_WORKING_DAYS){
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case IS_FULL_TIME:
@@ -20,10 +21,10 @@ public class EmployeeWageBuilder {
                     empHrs = 0;
             }
 
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            System.out.println("Employee Wage:" + empWage);
+            totalEmpHours +=empHrs;
+            System.out.println("Day#: " +totalWorkingDays + " Emp hrs: "+  totalEmpHours);
         }
+        int totalEmpWage=totalEmpHours*EMP_RATE_PER_HOUR;
         System.out.println("Total wage: "+ totalEmpWage);
     }
 }
