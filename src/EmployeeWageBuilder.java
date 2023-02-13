24dfbebc2b3554d73d6702b8199e5d93a99ce4c2
subzero <shubham8111994@gmail.com>
2023-02-13 21:17:56 +0530

    added employee total wage using method

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -1,14 +1,17 @@
 public class EmployeeWageBuilder {
-    public static final int IS_FULL_TIME =1;
-    public static final int IS_PART_TIME =2;
-    public static final int EMP_RATE_PER_HOUR=20;
-    public static final int NO_OF_WORKING_DAYS=20;
-    public static final int MAX_HOURS_PER_MONTH= 100;
-    public static void main(String[] args) {
-        int empHrs=0;
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NO_OF_WORKING_DAYS = 20;
+    public static final int MAX_HOURS_PER_MONTH = 100;
+
+    public static int computeWage()
+
+    {
+        int empHrs = 0;
         int totalEmpHours = 0;
-        int totalWorkingDays=0;
-        while (totalEmpHours<=MAX_HOURS_PER_MONTH && totalWorkingDays<NO_OF_WORKING_DAYS){
+        int totalWorkingDays = 0;
+        while (totalEmpHours <= MAX_HOURS_PER_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS) {
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case IS_FULL_TIME:
@@ -21,10 +24,15 @@ public class EmployeeWageBuilder {
                     empHrs = 0;
             }
 
-            totalEmpHours +=empHrs;
-            System.out.println("Day#: " +totalWorkingDays + " Emp hrs: "+  totalEmpHours);
+            totalEmpHours += empHrs;
+            System.out.println("Day#: " + totalWorkingDays + " Emp hrs: " + totalEmpHours);
         }
-        int totalEmpWage=totalEmpHours*EMP_RATE_PER_HOUR;
-        System.out.println("Total wage: "+ totalEmpWage);
+        int totalEmpWage = totalEmpHours * EMP_RATE_PER_HOUR;
+        System.out.println("Total wage: " + totalEmpWage);
+        return totalEmpWage;
+    }
+
+    public static void main(String[] args) {
+        computeWage();
     }
 }
