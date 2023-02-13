a78c24b9489ce06c107c98629eed888d29265c7b
subzero <shubham8111994@gmail.com>
2023-02-13 20:50:56 +0530

    added employee daily wage

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -1,10 +1,16 @@
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
-        int IS_Full_TIME =1;
+        int IS_FULL_TIME =1;
+        int EMP_RATE_PER_HOUR=20;
+
+        int empHrs=0;
+        int empWage=0;
         double empCheck =Math.floor(Math.random() * 10) % 2;
-        if(empCheck == IS_Full_TIME)
-            System.out.println("Employee is present");
+        if(empCheck == IS_FULL_TIME)
+            empHrs=8;
         else
-            System.out.println("Employee is absent");
+            empHrs=0;
+        empWage=empHrs*EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage:" + empWage);
     }
 }
