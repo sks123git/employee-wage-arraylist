b9becfbe3d2577cc373545aa72e2105526726db0
subzero <shubham8111994@gmail.com>
2023-02-13 20:45:35 +0530

    added employee check

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -1,5 +1,10 @@
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int IS_Full_TIME =1;
+        double empCheck =Math.floor(Math.random() * 10) % 2;
+        if(empCheck == IS_Full_TIME)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
     }
 }
